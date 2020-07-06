b10aa5a757dfe147ba40da14319fd3268b295a82
admin <admin@asus>
2020-07-06 08:31:02 +0530

    First commit

---
+++ actiTIME/src/test/java/actiTIME/Demo.java
@@ -0,0 +1,13 @@
+package actiTIME;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void run()
+	{
+		Reporter.log("My test",true);
+	}
+
+}
