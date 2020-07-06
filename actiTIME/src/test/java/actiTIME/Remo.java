04f7f7a6a5ab2074513006793f51338adcbdf250
admin <admin@asus>
2020-07-06 08:37:00 +0530

    My commit

---
+++ actiTIME/src/test/java/actiTIME/Remo.java
@@ -0,0 +1,13 @@
+package actiTIME;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Remo {
+	@Test
+	public void run()
+	{
+		Reporter.log("test run",true);
+	}
+
+}
